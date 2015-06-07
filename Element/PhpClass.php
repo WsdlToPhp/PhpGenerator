fa3ec7e616b001ec427e286206dddc1758b40546
Mikael DELSOL <delsol.mikael@gmail.com>
2015-06-07 11:05:42 +0200

    add sanity check

--- Element/PhpClass.php
+++ Element/PhpClass.php
@@ -91,7 +91,7 @@ class PhpClass extends AbstractElement
      */
     public static function extendsIsValid($extends)
     {
-        return $extends === null || (is_string($extends) && !empty($extends)) || $extends instanceof PhpClass;
+        return $extends === null || (is_string($extends) && !empty($extends) && self::nameIsValid($extends)) || $extends instanceof PhpClass;
     }
     /**
      * @return string|PhpClass
@@ -139,7 +139,7 @@ class PhpClass extends AbstractElement
      */
     public static function interfaceIsValid($interface)
     {
-        return (is_string($interface) && !empty($interface)) || $interface instanceof PhpClass;
+        return (is_string($interface) && !empty($interface) && self::nameIsValid($interface)) || $interface instanceof PhpClass;
     }
     /**
      *
